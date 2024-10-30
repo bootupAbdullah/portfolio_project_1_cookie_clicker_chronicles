6d94ba1a520999c9ea4d08223e756f0acbe83a4d
Abdulla Durrani <Abdullahkd@gmail.com>
2024-10-30 16:19:32 -0400

    completed MVP

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log('hello')
+//variables===========================================================
+const gridContainerItems = document.querySelectorAll('.grid-items');
+const totalCLicksCount = document.querySelector('#total-clicks-count');
+let clickCount = 0
+// Console Outputs ======================================================
+console.dir(gridContainerItems)
+console.dir(totalCLicksCount)
+
+
+// Functions ======================================================
+
+// buttons.forEach((button) => {
+//     button.addEventListener('click', (event) => {
+//     display.innerText = button.innerText
+//     console.log(event.target.innerText);
+//     });
+//   });
+
+
+gridContainerItems.forEach((quadrant) => {
+    quadrant.addEventListener('click', (event) => {
+        clickCount += 1
+        totalCLicksCount.innerText = clickCount
+    });
+});
+
